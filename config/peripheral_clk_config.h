bc2e015a816217457f83dec748e64440bdae4b6c
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-17 17:09:34 +0000

    Frequency: 458752 (458 khz). Calculated by Atmel Start Subtle atmel start authored changes - on test 	modified:   config/peripheral_clk_config.h On branch atmel_start_bb

--- config/peripheral_clk_config.h
+++ config/peripheral_clk_config.h
@@ -9,7 +9,7 @@
  * \brief CPU's Clock frequency
  */
 #ifndef CONF_CPU_FREQUENCY
-#define CONF_CPU_FREQUENCY 0
+#define CONF_CPU_FREQUENCY 458752
 #endif
 
 // <y> Core Clock Source
@@ -65,7 +65,7 @@
  * \brief SERCOM5's Core Clock frequency
  */
 #ifndef CONF_GCLK_SERCOM5_CORE_FREQUENCY
-#define CONF_GCLK_SERCOM5_CORE_FREQUENCY 0
+#define CONF_GCLK_SERCOM5_CORE_FREQUENCY 458752
 #endif
 
 /**
