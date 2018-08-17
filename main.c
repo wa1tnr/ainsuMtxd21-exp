36f48054fca21d8a5ec5689c43e2cdc1a3be61eb
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-17 17:00:02 +0000

    Small experiments.  blink added. 	modified:   main.c On branch test__32_kHz_basic-aa-

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include <atmel_start.h>
 #include "pins.h"
 
+void waste(void) { // time and resources
+    for (volatile int i=(8 * 2104); i>0; i--) {
+    }
+}
+
+void blink(void) {
+    PORT->Group[PORTA].OUTTGL.reg  = (uint32_t)(1 << 17); // PA17 //    13 pintoggle // D13
+    waste();
+    PORT->Group[PORTA].OUTTGL.reg  = (uint32_t)(1 << 17);
+    waste(); waste();
+}
+
 int main(void) {
     /* Initializes MCU, drivers and middleware */
     pins_setup();
 
     /* Replace with your application code */
     while (1) {
+        blink();
         toggle_d11();
     }
 }
-
