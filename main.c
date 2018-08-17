6a75efc15eed98e55707663dd1e877bec5684aaf
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-17 15:46:40 +0000

    Formatting. 	modified:   main.c On branch test__32_kHz_basic-aa-

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <atmel_start.h>
 #include "pins.h"
 
-int main(void)
-{
-	/* Initializes MCU, drivers and middleware */
-        pins_setup();
+int main(void) {
+    /* Initializes MCU, drivers and middleware */
+    pins_setup();
 
-	/* Replace with your application code */
-	while (1) {
-            toggle_d11();
-	}
+    /* Replace with your application code */
+    while (1) {
+        toggle_d11();
+    }
 }
+
