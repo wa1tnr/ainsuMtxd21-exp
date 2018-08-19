e66ebdcc27c87158e29fe336511600a463317af9
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-19 01:36:56 +0000

    On branch Hello_Data_Dumper_EXP_aa A bit better - two hex digits for each.
    Hard to tell what's going on.
    	modified:   dump.c

--- src/dump.c
+++ src/dump.c
@@ -10,9 +10,10 @@
 
 void say_something(void) { }
 
-// #define LBOUND 0x2040
-#define LBOUND 0x2070
-#define UBOUND  0x420
+// #define LBOUND 0x2070
+// #define LBOUND 0x2010
+#define LBOUND 0x1280
+#define UBOUND  0x1420
 
 /* dump 16 bytes of RAM in hex with ascii on the side */
 uint8_t* srdump(void) {
@@ -74,7 +75,7 @@ uint8_t* srdump(void) {
 
 char byte_r;
 
-uint8_t* parsed(void) { // parse it out
+uint8_t* parsed_low(void) { // parse it out
     int rbyte = 0;
 
     // char byte_r;
@@ -85,39 +86,82 @@ uint8_t* parsed(void) { // parse it out
 
     rbyte = byte_s; // rbyte is a working copy;
 
-    rbyte = byte_s && 0xf0; // mask lower nybble
+    rbyte = byte_s & 0x0f; // mask upper nybble - two ampersands gives a boolean
 
     // if (rbyte == 0) return (uint8_t *) rbyte; // good
 
-    if (rbyte == 0) io_write(io, (uint8_t *)"\060\060", 2); // '0'
-    if (rbyte == 1) io_write(io, (uint8_t *)"\060\061", 2); // '1'
-    if (rbyte == 2) io_write(io, (uint8_t *)"\060\062", 2); // '2'
-    if (rbyte == 3) io_write(io, (uint8_t *)"\060\063", 2); // '3'
-    if (rbyte == 4) io_write(io, (uint8_t *)"\060\064", 2); // '4'
-    if (rbyte == 5) io_write(io, (uint8_t *)"\060\065", 2); // '5'
-    if (rbyte == 6) io_write(io, (uint8_t *)"\060\066", 2); // '0'
-    if (rbyte == 7) io_write(io, (uint8_t *)"\060\067", 2); // '0'
-    if (rbyte == 8) io_write(io, (uint8_t *)"\060\068", 2); // '0'
-    if (rbyte == 9) io_write(io, (uint8_t *)"\060\069", 2); // '0'
-    if (rbyte == 0xa) io_write(io, (uint8_t *)"A", 2); // 'A'
-    if (rbyte == 0xb) io_write(io, (uint8_t *)"B", 2); // 'B'
-    if (rbyte == 0xc) io_write(io, (uint8_t *)"C", 2); // 'C'
-    if (rbyte == 0xd) io_write(io, (uint8_t *)"D", 2); // 'D'
-    if (rbyte == 0xe) io_write(io, (uint8_t *)"E", 2); // 'E'
-    if (rbyte == 0xf) io_write(io, (uint8_t *)"F", 2); // 'F'
+    if (rbyte == 0) io_write(io, (uint8_t *)"0", 1); // '0'
+    if (rbyte == 1) io_write(io, (uint8_t *)"1", 1); // '1'
+    if (rbyte == 2) io_write(io, (uint8_t *)"2", 1); // '2'
+    if (rbyte == 3) io_write(io, (uint8_t *)"3", 1); // '3'
+    if (rbyte == 4) io_write(io, (uint8_t *)"4", 1); // '4'
+    if (rbyte == 5) io_write(io, (uint8_t *)"5", 1); // '5'
+    if (rbyte == 6) io_write(io, (uint8_t *)"6", 1); // '0'
+    if (rbyte == 7) io_write(io, (uint8_t *)"7", 1); // '0'
+    if (rbyte == 8) io_write(io, (uint8_t *)"8", 1); // '0'
+    if (rbyte == 9) io_write(io, (uint8_t *)"9", 1); // '0'
+    if (rbyte == 0xa) io_write(io, (uint8_t *)"A", 1); // 'A'
+    if (rbyte == 0xb) io_write(io, (uint8_t *)"B", 1); // 'B'
+    if (rbyte == 0xc) io_write(io, (uint8_t *)"C", 1); // 'C'
+    if (rbyte == 0xd) io_write(io, (uint8_t *)"D", 1); // 'D'
+    if (rbyte == 0xe) io_write(io, (uint8_t *)"E", 1); // 'E'
+    if (rbyte == 0xf) io_write(io, (uint8_t *)"F", 1); // 'F'
 
     // spacing kludge, temporary:
-    io_write(io, (uint8_t *)"\040", 1); // SPACE ASCII 32 0x20
+    // io_write(io, (uint8_t *)"\040", 1); // SPACE ASCII 32 0x20
+    // pp thing went here
 
     return (uint8_t *) rbyte;
 }
+
+uint8_t* parsed_hi(void) { // parse it out
+    int rbyte = 0;
+
+    // char byte_r;
+    // char c = *ram++;
+    // byte_r = (uint8_t) c; // global
+
+    char byte_s = byte_r;
+
+    rbyte = byte_s; // rbyte is a working copy;
+
+    rbyte = byte_s & 0xf0; // mask lower nybble
+
+    rbyte = rbyte >> 4;
+
+    // if (rbyte == 0) return (uint8_t *) rbyte; // good
+
+    if (rbyte == 0) io_write(io, (uint8_t *)"\060", 1); // '0'
+    if (rbyte == 1) io_write(io, (uint8_t *)"\061", 1); // '1'
+    if (rbyte == 2) io_write(io, (uint8_t *)"\062", 1); // '2'
+    if (rbyte == 3) io_write(io, (uint8_t *)"\063", 1); // '3'
+    if (rbyte == 4) io_write(io, (uint8_t *)"\064", 1); // '4'
+    if (rbyte == 5) io_write(io, (uint8_t *)"\065", 1); // '5'
+    if (rbyte == 6) io_write(io, (uint8_t *)"\066", 1); // '0'
+    if (rbyte == 7) io_write(io, (uint8_t *)"\067", 1); // '0'
+    if (rbyte == 8) io_write(io, (uint8_t *)"\068", 1); // '0'
+    if (rbyte == 9) io_write(io, (uint8_t *)"\069", 1); // '0'
+    if (rbyte == 0xa) io_write(io, (uint8_t *)"A", 1); // 'A'
+    if (rbyte == 0xb) io_write(io, (uint8_t *)"B", 1); // 'B'
+    if (rbyte == 0xc) io_write(io, (uint8_t *)"C", 1); // 'C'
+    if (rbyte == 0xd) io_write(io, (uint8_t *)"D", 1); // 'D'
+    if (rbyte == 0xe) io_write(io, (uint8_t *)"E", 1); // 'E'
+    if (rbyte == 0xf) io_write(io, (uint8_t *)"F", 1); // 'F'
+
+    // spacing kludge, temporary:
+    io_write(io, (uint8_t *)"\040", 1); // SPACE ASCII 32 0x20
+    // pp thing went here
+    return (uint8_t *) rbyte;
+}
+
+int COUNTER = 0;
 
 /* dump 16 bytes of RAM in hex with ascii on the side */
 uint8_t* cdump(void) {
     char buffer[5] = "";
     char *ram;
     // int p = pop();
-    int p = LBOUND ;
+    int p = LBOUND+COUNTER ;
     ram = (char*)p;
 
  // sprintf(buffer, "%4x", p);
@@ -136,30 +180,43 @@ uint8_t* cdump(void) {
 
  // Serial.print("   ");
 
-    io_write(io, (uint8_t *)"  ", 3);
+    io_write(io, (uint8_t *)"--", 2);
 
     for (int i = 0; i < 16; i++) {
         char c = *ram++;
 
         byte_r = (uint8_t) c; // global
 
-        parsed(); // print upper nybble out serial port
+        parsed_hi(); // print upper nybble out serial port
+
+        parsed_low(); // print lower nybble out serial port
+
 
      // sprintf(buffer, " %2x", (c & 0xff));
-        strcpy(buffer, (char *) (c & 0xff));
+
+     // strcpy(buffer, (char *) (c & 0xff));
+
      // Serial.print(buffer);
-        io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
+
+     // io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
+
+
     }
     ram = (char*)p;
  // Serial.print("   ");
-    io_write(io, (uint8_t *)"  ", 3);
+    io_write(io, (uint8_t *)"__", 2);
     for (int i = 0; i < 16; i++) {
         buffer[0] = *ram++;
-        if (buffer[0] > 0x7f || buffer[0] < ' ') buffer[0] = '.';
+        if (buffer[0] > 0x7f || buffer[0] < ' ') buffer[0] = '.'; // the dot for unprintable chars
         buffer[1] = '\0';
      // Serial.print(buffer);
+
+     // print the ascii translation chars on the right margin, one
+     // at a time:
+
         io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
     }
  // push(p + 16);
+    COUNTER = COUNTER + 16;
     return (uint8_t *)p;
 }
