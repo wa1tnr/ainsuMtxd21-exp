a067b4cf475b0d81055e75823467166e3584866a
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-21 21:27:47 +0000

    Comments and minor output formatting stuph addressed.
    This one's in the bag - good program.
    
    	modified:   src/dump.c
    On branch KM_converser_aa-

--- src/dump.c
+++ src/dump.c
@@ -1,15 +1,33 @@
 /* dump.c */
-/* 21 Aug 2018 00:51 UTC */
+/* 21 Aug 2018 21:23 UTC */
 /* wa1tnr, Connecticut USA */
 /* LICENSE: LGPL v2.1 */
+
+// On branch KM_converser_aa-
 
 #include "driver_examples.h"
 #include "common.h"
 #include <string.h>
 
 // lower boundary of the memory to examine
-#define LBOUND 0x1d10+0x10
-#define UBOUND  0x1420
+// #define LBOUND 0x1d10+0x10
+
+// #define LBOUND 0x20000000+(0x0100 * 2)
+
+// SRAM     // #define LBOUND 0x20000000+(0x0100 * 0)
+// OTHER    // #define LBOUND 0x40000000+(0x0100 * 0)
+
+// change the last number to any positive integer, for an offset:
+// #define LBOUND 0x20000000+(0x0100 * 0)
+
+#define LBOUND 0x1d10+0x10 // Adafruit
+
+/* see main.c for how many lines of memory to dump.
+   It may be a very large number!
+
+   dump.c  on the other hand provides a memory dump,
+           of a single line of 16 bytes, when asked
+           to do so. */
 
 uint32_t  rbyte = 0;
 char      byte_r;
@@ -44,7 +62,7 @@ uint8_t* parsed_low(void) {
     rbyte = byte_s; // rbyte is a working copy;
     rbyte = byte_s & 0x0f; // mask upper nybble - two ampersands gives a boolean
     new_ascii_emit(); // ascii_emit();
-    io_write(io, (uint8_t *)"\040", 1); // SPACE ASCII 32 0x20
+ // io_write(io, (uint8_t *)"\040", 1); // SPACE ASCII 32 0x20
     return (uint8_t *) rbyte;
 }
 
@@ -68,10 +86,9 @@ uint8_t* cdump(void) {
     ram = (char*)p;
 
     io_write(io, (uint8_t *)"\015\012", 2); // CRLF
-    io_write(io, (uint8_t *)"  ", 2);
+ // io_write(io, (uint8_t *)"  ", 2);
 
     // address printing
-
 
     uint32_t adrs  = (uint32_t) ram;
 
@@ -92,6 +109,9 @@ uint8_t* cdump(void) {
     parsed_hi();
     parsed_low();
 
+    // may not want this here, formatting style decision:
+    // io_write(io, (uint8_t *)":", 1);
+
     /* stanza */
     digit = adrs & 0x0000ff00;
     digit = digit >>  8;
@@ -106,7 +126,7 @@ uint8_t* cdump(void) {
     parsed_hi();
     parsed_low();
 
-    io_write(io, (uint8_t *)":: ", 3);
+    io_write(io, (uint8_t *)" ", 1);
 
     byte_r = 0; // clear it for next subprogram's reuse
 
@@ -118,6 +138,7 @@ uint8_t* cdump(void) {
         parsed_hi(); // print upper nybble out serial port
 
         parsed_low(); // print lower nybble out serial port
+        _spc();
 
         if (i == 7) _spc();
     } // for
@@ -125,14 +146,15 @@ uint8_t* cdump(void) {
     ram = (char*)p;
 
 
-    io_write(io, (uint8_t *)"  ", 2);
+    io_write(io, (uint8_t *)"   ", 3); // white space column separating hex from ASCII
 
     /* FEATURE
     _bkbar(); // begin stanza
     */
     for (int i = 0; i < 16; i++) {
         buffer[0] = *ram++;
-        if (buffer[0] > 0x7f || buffer[0] < ' ') buffer[0] = '.'; // the dot for unprintable chars
+        // 0x7f is unprintable, so 0x7e
+        if (buffer[0] > 0x7e || buffer[0] < ' ') buffer[0] = '.'; // the dot for unprintable chars
         buffer[1] = '\0';
 
      // print the ascii translation chars on the right margin, one
