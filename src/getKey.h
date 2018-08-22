0a1331311eee79721228e429f386f482e136695f
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-22 17:57:58 +0000

    io_read() moved into getKey.c - corresponding include file 	new file:   src/getKey.h On branch xKM_converser_aa-br-bb-

---
+++ src/getKey.h
@@ -0,0 +1,10 @@
+// Tue Aug 21 21:23:47 UTC 2018
+/* wa1tnr - August, 2018 LGPL v2.1 */
+
+// On branch KM_converser_aa-
+//     On branch xKM_converser_aa-br-bb-
+
+// #define maxtib 16
+extern char tib[maxtib];
+// extern void USART_read(void); // hardware implementation-dependent
+extern void getKey(void); // hardware-independent wrapper
