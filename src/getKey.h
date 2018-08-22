c38c593a11fd69dc1da60fa30f5b6cf018e9bff2
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-22 18:06:40 +0000

    include for getKey.c 	modified:   getKey.h On branch xKM_converser_aa-br-bb-

--- src/getKey.h
+++ src/getKey.h
@@ -1,10 +1,8 @@
-// Tue Aug 21 21:23:47 UTC 2018
+// Wed Aug 22 18:00:54 UTC 2018
 /* wa1tnr - August, 2018 LGPL v2.1 */
 
 // On branch KM_converser_aa-
 //     On branch xKM_converser_aa-br-bb-
 
-// #define maxtib 16
-extern char tib[maxtib];
-// extern void USART_read(void); // hardware implementation-dependent
+extern char tib[maxtib]; /* Terminal Input Buffer for interpreter */
 extern void getKey(void); // hardware-independent wrapper
