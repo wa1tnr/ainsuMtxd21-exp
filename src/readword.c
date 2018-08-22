4132d7e1208abb5c382b827e56bc74a8a35a83f2
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-22 21:00:41 +0000

    Convert ch[0] to uint8_t and print all printable characters
    	modified:   readword.c
    
    Simple typewriter.  Type a period between words
    so.that.it.looks.like.this to have some fun with itt.
    
    On branch xKM_converser_aa-br-cc-

--- src/readword.c
+++ src/readword.c
@@ -1,7 +1,12 @@
 #include "common.h"
 #include "getKey.h"
+#include "driver_examples.h"
 
 void printing(void) {
+    uint8_t ch_read = (uint32_t) ch[0];
+    if (ch_read != 32) { // not a space delimiter
+        io_write(io, (uint8_t *) ch, 1);
+    }
 }
 
 uint8_t reading(void) {
