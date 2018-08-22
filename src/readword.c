172f7cd1316fcba644c14cf4fe76ded0e9cff668
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-22 21:07:03 +0000

    The rest of the simplified REPL (minus Eval!) routine.
    	modified:   readword.c
    
    The forth input stream parses on word (or number) boundaries.
    
    SPACE (ascii 32 0x20) and ENTER (ascii 13 0x0d) are delimiters
    in this forth, marking such boundaries (think of pressing ENTER
    as a special case of pressing the SPACE BAR -- outcomes are
    similar, in several ways).
    
    Echoing the ENTER keystroke to the console isn't done quite
    the same way as one might expect (the ok prompt may need to
    be printed, first, for example).

--- src/readword.c
+++ src/readword.c
@@ -4,8 +4,15 @@
 
 void printing(void) {
     uint8_t ch_read = (uint32_t) ch[0];
-    if (ch_read != 32) { // not a space delimiter
-        io_write(io, (uint8_t *) ch, 1);
+
+    if (ch_read == 13) {
+        _spc(); // do not print a cr here!
+    } else {
+        if (ch_read != 32) { // a space delimiter
+            io_write(io, (uint8_t *) ch, 1);
+        } else { // it's a space
+            _spc();
+        }
     }
 }
 
